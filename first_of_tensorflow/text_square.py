7cc4323366f8903e178bd936e824066d4be297c9
Benjamin <305193188@qq.com>
2018-10-29 22:08:19 +0800

    特征缩放

--- first_of_tensorflow/text_square.py
+++ first_of_tensorflow/text_square.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 # 创建x_data和y_data,使用numpy生成随机点，并且生成噪声noise
 x_data = np.linspace(-0.5, 0.5, 200, dtype=np.float32)[:, np.newaxis]
 noise = np.random.normal(0, 0.03, x_data.shape).astype(np.float32)
-# y_data = 2.4 * np.square(x_data) + 1.5 * x_data + 0.9 + noise
-y_data = np.square(x_data) + noise
+# 特征缩放
+y_data = (2.4 * np.square(x_data) + 1.5 * x_data + 0.9 + noise)/18
 
 # 传入和传出的数据
 x = tf.placeholder(np.float32, [None, 1])
@@ -30,7 +30,7 @@ L1 = tf.nn.tanh(Wx_plus_b_L1)
 Weight_L2 = tf.Variable(tf.random_normal([10, 1]))
 biases_L2 = tf.Variable(np.zeros([1, 1]).astype(np.float32))
 Wx_plus_b_L2 = tf.matmul(L1, Weight_L2) + biases_L2
-answer = tf.nn.tanh(Wx_plus_b_L2)
+answer = tf.nn.relu(Wx_plus_b_L2)
 
 # 代价函数和梯度下降法训练
 loss = tf.reduce_mean(tf.square(y - answer))
@@ -40,10 +40,10 @@ train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
-    for i in range(5001):
+    for i in range(20001):
         sess.run(train_step, feed_dict={x: x_data, y: y_data})
-        if i % 500 == 0:
-            print("After %d steps, loss is %1.10f" % (i, sess.run(loss, feed_dict={x: x_data, y: y_data})))
+        if i % 1000 == 0:
+            print("After %4d steps, loss is %1.10f" % (i, sess.run(loss, feed_dict={x: x_data, y: y_data})))
     plt.figure()
     plt.scatter(x_data, y_data)
     plt.plot(x_data, sess.run(answer, feed_dict={x: x_data, y: y_data}), "r-", lw=5)
